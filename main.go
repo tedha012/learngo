817fee569ce937995563bcfc54e6e73cd883255d
tedha012 <tedha012@gmail.com>
2022-10-14 17:38:38 +0900

    Arrays and Slices

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	*b = 20
-	fmt.Println(a)
+	names := []string{"nico", "lyn", "dal"}
+	names = append(names, "flynn")
+	fmt.Println(names)
 }
