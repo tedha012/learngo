1e6e57cb2dda150def7ce3250baa248fe461ec6a
TedHa <tedha012@gmail.com>
2022-10-25 22:42:00 +0900

    #2.1-2 Methods

--- main.go
+++ main.go
@@ -8,5 +8,11 @@ import (
 
 func main() {
 	account := accounts.NewAccount("nico")
-	fmt.Println(account)
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
