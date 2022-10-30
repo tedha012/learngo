b072ab2f483a36bcbd75c89e29b640b119a5486f
TedHa <tedha012@gmail.com>
2022-10-30 12:30:07 +0900

    #3.3 Channels

--- main.go
+++ main.go
@@ -6,14 +6,17 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	go sexyCount("ted")
-	time.Sleep(time.Second * 5)
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is Sexy", i)
-		time.Sleep(time.Second)
-	}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
