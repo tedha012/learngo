301a41f95b4a56918b8d95732a94e77a47da5121
TedHa <tedha012@gmail.com>
2022-10-30 12:40:39 +0900

    #3.4 Channels Recap

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-	c := make(chan bool)
+	c := make(chan string)
 	people := [2]string{"nico", "flynn"}
 	for _, person := range people {
 		go isSexy(person, c)
+		fmt.Println("Received this message", <-c)
 	}
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+
 }
 
-func isSexy(person string, c chan bool) {
+func isSexy(person string, c chan string) {
 	time.Sleep(time.Second * 5)
 	fmt.Println(person)
-	c <- true
+	c <- person + " is sexy"
 }
